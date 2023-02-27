1a6c46a1f143a0fcd4c17148438d3d87ce8f0f8a
Tamule <122865779+Tamule@users.noreply.github.com>
2023-02-27 02:08:15 +0200

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default function Weather() {
+  function handleClick() {
+    alert("Coming soon...");
+  }
+
+  return (
+    <div className="Weather">
+      <button onClick={handleClick}>Current Temperature</button>
+    </div>
+  );
+}
